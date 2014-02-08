2bc4af30c9bc47ff356955b0e5eb2257a7e8e5dc
Mark Pundsack <mp@heroku.com>
2014-02-07 16:56:34 -0800

    Add Hello World message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RubyRailsSample::Application.routes.draw do
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
